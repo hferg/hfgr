2f74580f419dbe6b66a19f18dc3c880cd28d4962
hferg <h.ferguson.gow@gmail.com>
2015-05-15 16:05:37 +0100

    Added multiple clade functionality to localLambda.R
    localLambda.R can now take a vector of node numbers and of lambdas and transform different parts of the tree by different lambdas.

--- R/localLambda.R
+++ R/localLambda.R
@@ -2,16 +2,23 @@
 #'
 #' A function that transforms a tree according to lambda, or a portion of a tree according to lambda.
 #' @param tree A tree of class phylo.
-#' @param node A node number of vector of tip labels describing the clade to be transformed.
-#' @param lambda The value of lambda by which to transform the tree.
+#' @param node A node number of vector of nodes describing the clade(s) to be transformed.
+#' @param lambda The value or values of lambda by which to transform the specified clade(s).
 
 localLambda <- function(tree, node, lambda) {
-  descs <- getDescs(tree, node)
-  trans.edges <- which(tree$edge[ ,2] %in% descs & tree$edge[ ,2] > length(tree$tip.label))
-  ht1 <- nodeHeights(tree)
-  tree$edge.length[trans.edges] <- lambda * tree$edge.length[trans.edges]
-  ht2 <- nodeHeights(tree)
-  tree$edge.length[-trans.edges] <- tree$edge.length[-trans.edges] + ht1[-trans.edges, 2] - ht2[-trans.edges, 2]
+  if (length(node) != length(scalar)) {
+    stop("node and scalar must be the same length")
+  }
+  
+  for (i in length(node)) {
+    descs <- getDescs(tree, node[i])
+    trans.edges <- which(tree$edge[ ,2] %in% descs & tree$edge[ ,2] > length(tree$tip.label))
+    ht1 <- nodeHeights(tree)
+    tree$edge.length[trans.edges] <- lambda[i] * tree$edge.length[trans.edges]
+    ht2 <- nodeHeights(tree)
+    tree$edge.length[-trans.edges] <- tree$edge.length[-trans.edges] + ht1[-trans.edges, 2] - ht2[-trans.edges, 2]
+  }
+  
   return(tree)
 }
 
