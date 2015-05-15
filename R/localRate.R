f4477dfe60a92627900505143a3a6c7daefb3e6b
hferg <h.ferguson.gow@gmail.com>
2015-05-15 16:02:12 +0100

    Added multiple rate functionality into localRate.R
    localRate.R will now accept a vector of node numbers and scalars, and can scale more than one part of the tree.

--- R/localRate.R
+++ R/localRate.R
@@ -2,18 +2,24 @@
 #'
 #' A function that transforms branch lengths according to a scalar.
 #' @param tree A tree of class phylo.
-#' @param node A node number of vector of tip labels describing the clade to be transformed.
+#' @param node A node number of vector of nodes describing the clade to be transformed.
 #' @param scalar The multiplier that rate is increased by in the clade specified by node.
 
 localRate <- function(tree, node, scalar) {
-  
-  if (node == length(tree$tip.label) + 1) {
-    tree$edge.length <- tree$edge.length * scalar
-  } else {
-  descs <- getDescs(tree, node)
-  trans.edges <- which(tree$edge[ ,2] %in% descs)
-  tree$edge.length[trans.edges] <- scalar * tree$edge.length[trans.edges]
-  tree$edge.length[which(tree$edge[ ,2] == node)] <- scalar * tree$edge.length[which(tree$edge[ ,2] == node)]
+  if (length(node) != length(scalar)) {
+    stop("node and scalar must be the same length")
+  }
+  for (i in 1:length(node)) {
+    
+    if (node[i] == length(tree$tip.label) + 1) {
+      tree$edge.length <- tree$edge.length * scalar
+    } else {
+    descs <- getDescs(tree, node[i])
+    trans.edges <- which(tree$edge[ ,2] %in% descs)
+    tree$edge.length[trans.edges] <- scalar[i] * tree$edge.length[trans.edges]
+    tree$edge.length[which(tree$edge[ ,2] == node[i])] <- scalar[i] * tree$edge.length[which(tree$edge[ ,2] == node[i])]
+    }
+    
   }
   
   return(tree)
