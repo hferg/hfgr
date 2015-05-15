b2f35e9f332f37bf9856a91a608de2f3762f2029
hferg <h.ferguson.gow@gmail.com>
2015-05-15 16:29:57 +0100

    localRates.R now accepts either a vector of nodes, or a list of vectors of tip labels as clade definitions.
    This seems to work fine - the function can take a tree, a vector of scalars, and then either a vecotr of node numbers, or a list of vectors of tip names, which are then converted into node numbers by getMRCA.

--- R/localRate.R
+++ R/localRate.R
@@ -3,28 +3,48 @@
 #' A function that transforms branch lengths according to a scalar.
 #' @param tree A tree of class phylo.
 #' @param node A node number of vector of nodes describing the clade to be transformed.
+#' @param tips A vector of tip names, or a list of vectors of tip names, that define the areas to be transformed. If used, node is ignored.
 #' @param scalar The multiplier that rate is increased by in the clade specified by node.
 
-localRate <- function(tree, node, scalar) {
-  if (length(node) != length(scalar)) {
-    stop("node and scalar must be the same length")
-  }
-  
-  for (i in 1:length(node)) {
-    
-    if (node[i] == length(tree$tip.label) + 1) {
-      tree$edge.length <- tree$edge.length * scalar
-    } else {
-    descs <- getDescs(tree, node[i])
-    trans.edges <- which(tree$edge[ ,2] %in% descs)
-    tree$edge.length[trans.edges] <- scalar[i] * tree$edge.length[trans.edges]
-    tree$edge.length[which(tree$edge[ ,2] == node[i])] <- scalar[i] * tree$edge.length[which(tree$edge[ ,2] == node[i])]
+localRate <- function(tree, node = NULL, tips = NULL, scalar) {
+ 
+  if (is.null(tips)) {
+    if (length(node) != length(scalar)) {
+      stop("node and scalar must be the same length")
     }
-    
+    for (i in 1:length(node)) {
+      
+      if (node[i] == length(tree$tip.label) + 1) {
+        tree$edge.length <- tree$edge.length * scalar[i]
+      } else {
+      descs <- getDescs(tree, node[i])
+      trans.edges <- which(tree$edge[ ,2] %in% descs)
+      tree$edge.length[trans.edges] <- scalar[i] * tree$edge.length[trans.edges]
+      tree$edge.length[which(tree$edge[ ,2] == node[i])] <- scalar[i] * tree$edge.length[which(tree$edge[ ,2] == node[i])]
+      }
+      
+    }
+  } else {
+    if (length(tips) != length(scalar)) {
+      stop("node and scalar must be the same length")
+    }
+    for (i in 1:length(tips)) {
+      node <- getMRCA(tree, tips[[i]])
+      
+      if (node == length(tree$tip.label) + 1) {
+        tree$edge.length <- tree$edge.length * scalar[i]
+      } else {
+      descs <- getDescs(tree, node)
+      trans.edges <- which(tree$edge[ ,2] %in% descs)
+      tree$edge.length[trans.edges] <- scalar[i] * tree$edge.length[trans.edges]
+      tree$edge.length[which(tree$edge[ ,2] == node[i])] <- scalar[i] * tree$edge.length[which(tree$edge[ ,2] == node[i])]
+      }
+      
+    }
   }
   
   return(tree)
 }
 
-
+plot(localRate(treesm, tips = tips, scalar = c(4, 4)))
 
