e94a111f10f6d7ca92bd634e20633416153da381
hferg <h.ferguson.gow@gmail.com>
2015-05-15 16:57:28 +0100

    Updated localLambda.R to be able to take a tips or nodes, and more than one of each.
    Also, the function is slightly re-written in the same way as localRate.R so that it only has the actual transform method written out once.

--- R/localLambda.R
+++ R/localLambda.R
@@ -2,21 +2,41 @@
 #'
 #' A function that transforms a tree according to lambda, or a portion of a tree according to lambda.
 #' @param tree A tree of class phylo.
-#' @param node A node number of vector of nodes describing the clade(s) to be transformed.
+#' @param node A node number or vector of nodes describing the clade(s) to be transformed.
+#' @param tips A vector, or list of vectors, of tip labels definining clade(s) to be transformed
 #' @param lambda The value or values of lambda by which to transform the specified clade(s).
 
-localLambda <- function(tree, node, lambda) {
-  if (length(node) != length(lambda)) {
-    stop("node and lambda must be the same length")
-  }
-  
-  for (i in 1:length(node)) {
-    descs <- getDescs(tree, node[i])
+localLambda <- function(tree, node = NULL, tips = NULL, lambda) {
+  lambdaTrans <- function(tree, node, lambda) {
+    descs <- getDescs(tree, node)
     trans.edges <- which(tree$edge[ ,2] %in% descs & tree$edge[ ,2] > length(tree$tip.label))
     ht1 <- nodeHeights(tree)
-    tree$edge.length[trans.edges] <- lambda[i] * tree$edge.length[trans.edges]
+    tree$edge.length[trans.edges] <- lambda * tree$edge.length[trans.edges]
     ht2 <- nodeHeights(tree)
     tree$edge.length[-trans.edges] <- tree$edge.length[-trans.edges] + ht1[-trans.edges, 2] - ht2[-trans.edges, 2]
+    return(tree)
+  }
+  
+  if (is.null(tips)) {
+   
+    if (length(node) != length(lambda)) {
+      stop("node and lambda must be the same length")
+    }
+    
+    for (i in 1:length(node)) {
+      tree <- lambdaTrans(tree, node[i], lambda[i])
+    }
+  } else {
+  
+    if (length(tips) != length(lambda)) {
+      stop("tips and lambda must be the same length")
+    }
+    
+    for (i in 1:length(tips)) {
+      node <- getMRCA(tree, tips[[i]])
+      tree <- lambdaTrans(tree, node, lambda[i])
+    }
+
   }
   
   return(tree)
