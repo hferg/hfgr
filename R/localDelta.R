70ca7ad6ac25b34bbb6459cf85bf34e853cf93f1
hferg <h.ferguson.gow@gmail.com>
2015-05-18 11:19:40 +0100

    Changed localLambda.R, localRate.R, localKappa.R and localDetla.R into internal functions with simplified arguments.
    These functions now just take tree, node, and parameter value and have been simplified into internal functions. A wrapper function that will feed a vector of nodes or list of tip-groups and parameters into whichever function the user feels like will be added to make code lighter, and operation smoother.

---
+++ R/localDelta.R
@@ -0,0 +1,28 @@
+#' localDelta
+#'
+#' A function that transforms a tree according to lambda, or a portion of a tree according to lambda.
+#' @param tree A tree of class phylo.
+#' @param node A node number describing the clade(s) to be transformed.
+#' @param delta The value or values of lambda by which to transform the specified clade(s).
+
+localDelta <- function(tree, node, delta) {
+  descs <- getDescs(tree, node)
+  descs <- c(descs, node)
+  trans.edges <- which(tree$edge[ ,1] %in% descs & tree$edge[ ,1] > length(tree$tip.label))
+  bls <- tree$edge.length[trans.edges]
+  times <- branching.times(tree)
+  times <- times[names(times) %in% descs]
+  original.length <- max(times)
+  times <- max(times) - times
+  res <- tree
+
+  
+  for (i in 1:length(trans.edges)) {
+    bl <- tree$edge.length[trans.edges[i]]
+    age <- age <- times[names(times) %in% tree$edge[trans.edges][i]]
+    res$edge.length[trans.edges[i]] <- (age + bl) ^ delta - age ^ delta
+  }
+  
+  return(res)
+}
+
--- R/localRate.R
+++ R/localRate.R
@@ -2,52 +2,14 @@
 #'
 #' A function that transforms branch lengths according to a scalar.
 #' @param tree A tree of class phylo.
-#' @param node A node number of vector of nodes describing the clade to be transformed.
-#' @param tips A vector of tip names, or a list of vectors of tip names, that define the areas to be transformed. If used, node is ignored.
+#' @param node A node number describing the clade to be transformed.
 #' @param scalar The multiplier that rate is increased by in the clade specified by node.
 
-localRate <- function(tree, node = NULL, tips = NULL, scalar) {
-  rateTrans <- function(tree, node, scalar) {
+localRate <- function(tree, node, scalar) {
     descs <- getDescs(tree, node)
+    descs <- c(node, descs)
     trans.edges <- which(tree$edge[ ,2] %in% descs)
     tree$edge.length[trans.edges] <- scalar * tree$edge.length[trans.edges]
-    tree$edge.length[which(tree$edge[ ,2] == node)] <- scalar * tree$edge.length[which(tree$edge[ ,2] == node)]
     return(tree)
   }
   
-  if (is.null(tips)) {
-   
-    if (length(node) != length(scalar)) {
-      stop("node and scalar must be the same length")
-    }
-    
-    for (i in 1:length(node)) {
-      
-      if (node[i] == length(tree$tip.label) + 1) {
-        tree$edge.length <- tree$edge.length * scalar[i]
-      } else {
-        tree <- rateTrans(tree, node[i], scalar[i])
-      }
-      
-    }
-  } else {
-    
-    if (length(tips) != length(scalar)) {
-      stop("tips and scalar must be the same length")
-    }
-    
-    for (i in 1:length(tips)) {
-      node <- getMRCA(tree, tips[[i]])
-      
-      if (node == length(tree$tip.label) + 1) {
-        tree$edge.length <- tree$edge.length * scalar[i]
-      } else {
-        tree <- rateTrans(tree, node, scalar[i])
-      }
-      
-    }
-  }
-  
-  return(tree)
-}
-
