04545b7489bf6bfda56f7f0936456ee7fb94e08c
hferg <h.ferguson.gow@gmail.com>
2015-05-19 11:19:45 +0100

    Added smartRbind.R
    A function I copied from stack overflow that will bind vectors of unequal length.

---
+++ R/smartRbind.R
@@ -0,0 +1,25 @@
+#' smartRbind 
+#'
+#' A function that will rbind vectors of different lengths and return a matrix, provided each vector element is named.
+#' @param A bunch of vectors.
+#' @export
+#' @examples
+#' do.call(smartRbind, list.of.vectors)
+
+smartRbind <- function (...) {
+  # from GSee http://stackoverflow.com/questions/17308551/do-callrbind-list-for-uneven-number-of-column
+  dargs <- list(...)
+
+  if (!all(vapply(dargs, is.vector, TRUE))) 
+      stop("all inputs must be vectors")
+
+  if (!all(vapply(dargs, function(x) !is.null(names(x)), TRUE))) 
+      stop("all input vectors must be named.")
+
+  all.names <- unique(names(unlist(dargs)))
+  out <- do.call(rbind, lapply(dargs, `[`, all.names))
+  colnames(out) <- all.names
+  out
+}
+
+
