5f140378665922b1b5cfb0df4b555b3b560598f5
hferg <h.ferguson.gow@gmail.com>
2015-05-26 10:50:29 +0100

    updated localKappa to rescale the tree.

--- R/localKappa.R
+++ R/localKappa.R
@@ -5,11 +5,22 @@
 #' @param node A node number describing the clade to be transformed.
 #' @param kappa The multiplier that rate is increased by in the clade specified by node.
 
-localKappa <- function(tree, node, kappa) {
-    descs <- getDescs(tree, node)
-    descs <- c(node, descs)
-    trans.edges <- which(tree$edge[ ,2] %in% descs)
-    tree$edge.length[trans.edges] <- tree$edge.length[trans.edges] ^ kappa
-    return(tree)
+# TODO(hfg): Add rescale option.
+
+localKappa <- function(tree, node, kappa, rescale = TRUE) {
+  descs <- getDescs(tree, node)
+  descs <- c(node, descs)
+  trans.edges <- which(tree$edge[ ,2] %in% descs)
+  originalsum <- sum(tree$edge.length[trans.edges])
+  res <- tree
+  res$edge.length[trans.edges] <- tree$edge.length[trans.edges] ^ kappa
+  
+  if (rescale) {
+    newsum <- sum(res$edge.length[trans.edges])
+    ratio <- originalsum / newsum
+    res$edge.length[trans.edges] <- res$edge.length[trans.edges] * ratio
   }
+  
+  return(res)
+}
 
