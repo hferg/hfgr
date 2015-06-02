2b2dfbda5397329757960db8ef448af463c15201
hferg <h.ferguson.gow@gmail.com>
2015-06-02 12:38:01 +0100

    Updated isDefined to be exported as a function.
    Updates to NAMESAPCE

--- R/isDefined.R
+++ R/isDefined.R
@@ -2,5 +2,6 @@
 #'
 #' Internal function that tests if a variable is defined (i.e. is not NULL).
 #' @param x The variable or object you want to test the existence of.
+#' @export
 
 isDefined <- function(x) !is.null(x)
