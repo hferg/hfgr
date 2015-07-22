670f48f285c036e3f0a60d135f64087654acddda
hferg <h.ferguson.gow@gmail.com>
2015-07-22 12:09:11 +0100

    Updated simuDatRate.R to allow for an option to standardise the data.

--- R/simuDatRate.R
+++ R/simuDatRate.R
@@ -3,21 +3,26 @@
 #' Simulate data over a phylogeny, or sub section of a phylogeny, in order to reflect variable rates.
 #' Based on adding a constant to the tip data branch by branch and weighting by branch length and does
 #' not need to stretch the phylogeny.
-
+#' @name simuDatRate
 #' @param tree
 #' @param node
 #' @param a The constant that is weighted by branch length and added to the data at the tips. Once the relationship between scalar and this is known, this can be scalar
 #' @param sig The standard deviation of the random component of the BM simulation. NOT sigma squared.
 #' @param dat If you want to use some data you already have and change it this way, put it here.
 #' @param method Either add values recursively by tail branch length (default), or alternative "path" - which adds values proportional to total path length.
+#' @param standardise Logical, whether or not to z-standardise the data. Defaults to TRUE
 #' @export
 
-simuDatRate <- function(tree, node, a, sig, dat = NULL, method = "recursive") {
+simuDatRate <- function(tree, node, a, sig, dat = NULL, method = "recursive", standardise = TRUE) {
   
   if (isDefined(dat)) {
     dat <- dat
   } else {
     dat <- rTraitCont(tree, model = "BM", sigma = sig)
+  }
+  
+  if (standardise == TRUE) {
+    dat <- scale(dat, center = TRUE, scale = TRUE)
   }
   
   transdat <- dat  
