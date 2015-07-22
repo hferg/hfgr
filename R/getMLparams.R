88bce41140a965ab2672aeb7bf13abab7cfec44e
hferg <h.ferguson.gow@gmail.com>
2015-07-22 16:35:25 +0100

    Added a small function to get ML params from logfile(s), and updates to namespace and documentation.

---
+++ R/getMLparams.R
@@ -0,0 +1,26 @@
+#' getMLparams
+#'
+#' A function that will return the ML estimates from the logfile of an ML bayestraits run.
+#' @param logfile The name of the logfile that needs to be examined, or vector of logfile names.
+#' @name getMLparams
+#' @export
+
+getMLparams <- function(logfile) {
+  if (length(logfile) == 1) {
+    raw <- readLines(logfile)
+    output <- do.call(rbind, strsplit(raw[grep("\\bTree No\\b", raw):length(raw)], "\t"))
+    colnames(output) <- output[1, ]
+    res <- output[c(2:nrow(output)), ]
+  } else {
+    res <- matrix(nrow = length(logfile), ncol = 4)
+    for (i in 1:length(logfile)) {
+      raw <- readLines(logfile[i])
+      output <- do.call(rbind, strsplit(raw[grep("\\bTree No\\b", raw):length(raw)], "\t"))
+      colnames(res) <- output[1, ]
+      res[i, ] <- output[c(2:nrow(output)), ]      
+    }
+  res <- data.frame(res)
+  }
+  return(res)
+}
+
