6087b0e1de13bbda87fa6d88cff425bfb809f3eb
hferg <h.ferguson.gow@gmail.com>
2015-07-22 16:41:50 +0100

    added log names to the output of get ml param

--- R/getMLparams.R
+++ R/getMLparams.R
@@ -12,12 +12,13 @@ getMLparams <- function(logfile) {
     colnames(output) <- output[1, ]
     res <- output[c(2:nrow(output)), ]
   } else {
-    res <- matrix(nrow = length(logfile), ncol = 4)
+    res <- matrix(nrow = length(logfile), ncol = 5)
     for (i in 1:length(logfile)) {
       raw <- readLines(logfile[i])
       output <- do.call(rbind, strsplit(raw[grep("\\bTree No\\b", raw):length(raw)], "\t"))
-      colnames(res) <- output[1, ]
-      res[i, ] <- output[c(2:nrow(output)), ]      
+      colnames(res) <- c("log", output[1, ])
+      res[i, 1] <- logfile[i]
+      res[i, c(2:ncol(res))] <- output[c(2:nrow(output)), ]
     }
   res <- data.frame(res)
   }
