91f36918250c028469779850cb04ec2b8a83508a
hferg <h.ferguson.gow@gmail.com>
2015-09-10 14:01:56 +0100

    LocalscalarPP is basically done now - and returns a lot of nice information all in one place making analysis a lot easier.

--- R/localscalarPP.R
+++ R/localscalarPP.R
@@ -8,17 +8,23 @@
 #' @export
 #' @name localscalarPP
 
-localscalarPP <- function(rjlog, tree, burnin = 0, thinning = 1) {
+localscalarPP <- function(rjlog, rjtrees, tree, burnin = 0, thinning = 1) {
   # load the sample of trees.
-  extree <- tree
+  extree <- ladderize(tree)
+  print("Loading log file.")
   rjout <- loadRJ(rjlog, burnin = burnin, thinning = thinning)
+  print("Loading posterior trees.")
+  posttrees <- read.nexus(rjtrees)
+  posttrees <- posttrees[burnin:length(posttrees)]
 
+  # Make a list to store descriptions of each scalar present in each iteration.
   ratesperit <- vector(mode = "list", length = nrow(rjout$rj_output))
 
   for (i in 1:nrow(rjout$rj_output)) {
 
     lastrates <- rjout$rj_output[i, !is.na(rjout$rj_output[i, ])]
     
+    # If the number of columns is seven, there are no scalars applied this generation.
     if (ncol(lastrates) == 7) {
       node <- NA
       scale <- NA
@@ -40,115 +46,235 @@ localscalarPP <- function(rjlog, tree, burnin = 0, thinning = 1) {
   }
 
   # Get per-branch statistics.
-  counts <- matrix(ncol = 13, nrow = nrow(extree$edge))
+  # Make a table called counts - this has one row per branch, the ancestor and descendant node, and then
+  # space for the number of scalars in total, the type of scalars, and the number of generations one or more scalar
+  # is applied, as well as space for those scalar types.
+  counts <- matrix(ncol = 45, nrow = nrow(extree$edge))
 
-  counts[ , 1] <- c(1:nrow(extree$edge))
-  counts[ , 2] <- extree$edge[ , 1]
-  counts[ , 3] <- extree$edge[ , 2]
-  counts[ , c(4:13)] <- 0
+  colnames(counts) <- c("branch", "ancNode", "descNode", "nTips", "start", "end", "mid", "orgBL", "newBL", "ratioBL", 
+      "itersScaled", "itersRatescaled", "itersDelta", "itersKappa", "itersLambda", 
+      "nScalar", "nRate", "nDelta", "nKappa", "nLambda",
+      "rangeRate", "lqRate", "uqRate", "meanRate", "medianRate", "modeRate",
+      "rangeDelta", "lqDelta", "uqDelta", "meanDelta", "medianDelta", "modeDelta",
+      "rangeKappa", "lqKappa", "uqKappa", "meanKappa", "medianKappa", "modeKappa",
+      "rangeLambda", "lqLambda", "uqLambda", "meanLambda", "medianLambda", "modeLambda", "species")
 
-  colnames(counts) <- c("branch", "ancNode", "descNode", "timesScaled", "timesRatescaled", "timesDelta", "timesKappa", "timesLambda", 
-    "nScalar", "nRate", "nDelta", "nKappa", "nLambda")
+  counts[ , "branch"] <- c(1:nrow(extree$edge))
+  counts[ , "ancNode"] <- extree$edge[ , 1]
+  counts[ , "descNode"] <- extree$edge[ , 2]
+  counts[ , "orgBL"] <- extree$edge.length
+  print("Calculating mean branch lengths.")
+  counts[ , "newBL"] <- meanBranches(reftree = extree, trees = posttrees)$meantree$edge.length
+  counts[ , "ratioBL"] <- counts[ , "newBL"] / counts[ , "orgBL"]
+
+  hts <- nodeHeights(extree)
+  hts <- round(abs(hts - max(hts)), 4)
+  counts[ , "start"] <- hts[ , 1]
+  counts[ , "end"] <- hts[ , 2]
+  counts[ , "mid"] <- mean(c(hts[ , 1]))
+  counts <- as.data.frame(counts)
+
+  for (i in 1:nrow(counts)) {
+    descs <- getDescs(extree, node = counts[i, "descNode"])
+    counts[i, "nTips"] <- sum(descs <= length(tree$tip.label))
+    if (counts[i, "nTips"] == 0) {
+      counts[i, "nTips"] <- 1
+    }
+    if (counts[i, "descNode"] <= length(extree$tip.label)) {
+      counts[i, "species"] <- extree$tip.label[counts[i, "descNode"]]
+    }
+    counts[i, "mid"] <- mean(c(hts[i, 1], hts[i, 2]))
+  }
+  
+  counts[ , c(9:44)] <- 0
+
+  # Make tables to store the individual deltas and whatever for each iteration.
+  # How to do this? I can't just have onc cell per branch per iteration - that's no good.
+  # These ought to be lists I think, with one element per branch. Then add to each list each 
+  # delta, or whatever. It will be slow to reasign, but I don't know how long it needs to be - potentially
+  # very long... Especially big trees, long runs etc.
+
+  rates <- vector(mode = "list", length = nrow(counts))
+  deltas <- vector(mode = "list", length = nrow(counts))
+  kappas <- vector(mode = "list", length = nrow(counts))
+  lambdas <- vector(mode = "list", length = nrow(counts))
 
   for (i in 1:length(ratesperit)) {
-    rates <- ratesperit[[i]]
-
+    print(paste("Iteration", i, "of", length(ratesperit)))
+    scalars <- ratesperit[[i]]
     # make a list of all the branches, as defined by descendent node, with a column of zeroes for any scalar, rate, delta, 
     # kappa and lambda. These become ones when a scalar is place.
-    comptable <- matrix(ncol = 6, nrow = nrow(extree$edge))
-    comptable[ , 1] <- extree$edge[ , 2]  
-    comptable[ , c(2:6)] <- 0
-    colnames(comptable) <- c("descNode", "totalscalar", "rate", "delta", "kappa", "lambda")
-    
-    for (j in 1:nrow(rates)) {
-      currentnode <- rates[j, "node"]
-      currenttrans <- rates[j, "nodebranchdelta"]
+     if (!all(is.na(ratesperit[[i]]))) {
+      # Generate the table to store the current scalar of the current iteration.
+      comptable <- matrix(ncol = 6, nrow = nrow(extree$edge))
+      comptable[ , 1] <- extree$edge[ , 2]  
+      comptable[ , c(2:6)] <- 0
+      colnames(comptable) <- c("descNode", "totalscalar", "rate", "delta", "kappa", "lambda")
       
-      taxa <- rjout$subtrees[rjout$subtrees$node == currentnode, ]
-      taxa <- taxa[ , !is.na(taxa)]
-      taxa <- taxa[c(4:length(taxa))]
-      
-      mrca <- getMRCA(extree, rjout$taxa[rjout$taxa[ , 1] %in% taxa, 2])
-      
-      if (currenttrans == "Node") {
-        descs <- c(getDescs(extree, mrca), mrca)
-        # Then find branches from descs and count.
-        counts[counts[, "descNode"] %in% descs , "nScalar"] <- counts[counts[, "descNode"] %in% descs , "nScalar"] + 1
-        rws <- comptable[ , 1] %in% descs
-        comptable[rws & comptable[ , "totalscalar"] == 0, "totalscalar"] <- 1
-        comptable[rws & comptable[ , "rate"] == 0, "rate"] <- 1
-      }
-      if (currenttrans == "Branch") {
-        # Just use the branch that ends in mrca.
-        descs <- mrca
-        rws <- comptable[ , 1] %in% descs
-        counts[counts[, "descNode"] == mrca , "nScalar"] <- counts[counts[, "descNode"] == mrca , "nScalar"] + 1
-        comptable[rws & comptable[ , "totalscalar"] == 0, "totalscalar"] <- 1
-        comptable[rws & comptable[ , "rate"] == 0, "rate"] <- 1
-      }
-      if (currenttrans == "Delta") {
-        descs <- getDescs(extree, mrca)
-        rws <- comptable[ , 1] %in% descs
-        counts[counts[, "descNode"] %in% descs , "nDelta"] <- counts[counts[, "descNode"] %in% descs , "nDelta"] + 1
-        comptable[rws & comptable[ , "totalscalar"] == 0, "totalscalar"] <- 1
-        comptable[rws & comptable[ , "delta"] == 0, "delta"] <- 1
-      }    
-      if (currenttrans == "Kappa") {
-        descs <- getDescs(extree, mrca)
-        rws <- comptable[ , 1] %in% descs
-        counts[counts[, "descNode"] %in% descs , "nKappa"] <- counts[counts[, "descNode"] %in% descs , "nKappa"] + 1
-        comptable[rws & comptable[ , "totalscalar"] == 0, "totalscalar"] <- 1
-        comptable[rws & comptable[ , "kappa"] == 0, "kappa"] <- 1
-      }    
-      if (currenttrans == "Lambda") {
-        descs <- getDescs(extree, mrca)
-        rws <- comptable[ , 1] %in% descs
-        counts[counts[, "descNode"] %in% descs , "nLambda"] <- counts[counts[, "descNode"] %in% descs , "nLambda"] + 1
-        comptable[rws & comptable[ , "totalscalar"] == 0, "totalscalar"] <- 1
-        comptable[rws & comptable[ , "lambda"] == 0, "lambda"] <- 1
-      }
-    
-    }
-
-    # Count up generations when scaling occurred.
-    tmp <- counts[ , "descNode"] %in% comptable[comptable[ , "totalscalar"] == 1]
-    counts[tmp, "timesScaled"] <- counts[tmp, "timesScaled"] + 1
-    
-    tmp <- counts[ , "descNode"] %in% comptable[comptable[ , "rate"] == 1]
-    counts[tmp, "timesRatescaled"] <- counts[tmp, "timesRatescaled"] + 1
-    
-    tmp <- counts[ , "descNode"] %in% comptable[comptable[ , "delta"] == 1]
-    counts[tmp, "timesDelta"] <- counts[tmp, "timesDelta"] + 1
-    
-    tmp <- counts[ , "descNode"] %in% comptable[comptable[ , "kappa"] == 1]
-    counts[tmp, "timesKappa"] <- counts[tmp, "timesKappa"] + 1
+      for (j in 1:nrow(scalars)) {
+        # Get the node the scalar applies to, and the type of scalar.
+        currentnode <- scalars[j, "node"]
+        currenttrans <- scalars[j, "nodebranchdelta"]
+        currentscale <- as.numeric(as.character(scalars[j, "scale"]))
         
-    tmp <- counts[ , "descNode"] %in% comptable[comptable[ , "lambda"] == 1]
-    counts[tmp, "timesLambda"] <- counts[tmp, "timesLambda"] + 1
+        # Find the taxa numbers that descend from the node that the scalar is applied too.
+        taxa <- rjout$subtrees[rjout$subtrees$node == currentnode, ]
+        taxa <- taxa[ , !is.na(taxa)]
+        taxa <- taxa[c(4:length(taxa))]
+        
+        # Find the MRCA of those taxa - now the node number (mrca) is in terms of a phylogeny as stored in ape.
+        mrca <- getMRCA(extree, rjout$taxa[rjout$taxa[ , 1] %in% taxa, 2])
+        
+        # Find out what type of scalar is currently under consideration.
+        if (currenttrans == "Node") {
+          # If it's a node, the descendents of that node, plus the branch leading to it, need to be considered.
+          descs <- c(getDescs(extree, mrca), mrca)
+          # Then find branches from descs and count.
+          counts[counts[, "descNode"] %in% descs , "nScalar"] <- counts[counts[, "descNode"] %in% descs , "nScalar"] + 1
+          counts[counts[, "descNode"] %in% descs , "nRate"] <- counts[counts[, "descNode"] %in% descs , "nRate"] + 1          
+          rws <- comptable[ , 1] %in% descs
+          comptable[rws & comptable[ , "totalscalar"] == 0, "totalscalar"] <- 1
+          comptable[rws & comptable[ , "rate"] == 0, "rate"] <- 1
+          # Finally record the rates.
+          for (k in which(rws)) {
+            rates[[k]] <- c(rates[[k]], currentscale)
+          }
+        }
+        if (currenttrans == "Branch") {
+          # Just use the branch that ends in mrca.
+          descs <- mrca
+          rws <- comptable[ , 1] %in% descs
+          counts[counts[, "descNode"] == mrca , "nScalar"] <- counts[counts[, "descNode"] == mrca , "nScalar"] + 1
+          counts[counts[, "descNode"] %in% descs , "nRate"] <- counts[counts[, "descNode"] %in% descs , "nRate"] + 1          
+          comptable[rws & comptable[ , "totalscalar"] == 0, "totalscalar"] <- 1
+          comptable[rws & comptable[ , "rate"] == 0, "rate"] <- 1
+          # Finally record the deltas.
+          for (k in which(rws)) {
+            rates[[k]] <- c(rates[[k]], currentscale)
+          }
+        }
+        if (currenttrans == "Delta") {
+          # Define the branches (as descendent nodes) that were scaled.
+          descs <- getDescs(extree, mrca)
+          # Get rows of the table that need to have the sount increased.
+          rws <- comptable[ , 1] %in% descs
+          # Increase the number of deltas for each node in this scalar by 1, in the counts table, and 
+          # the total number of scalars.
+          counts[counts[, "descNode"] %in% descs , "nScalar"] <- counts[counts[, "descNode"] %in% descs , "nScalar"] + 1
+          counts[counts[, "descNode"] %in% descs , "nDelta"] <- counts[counts[, "descNode"] %in% descs , "nDelta"] + 1
+          # Then adjust the comptable to a) record yes or no for being scaled at all, and b) record 
+          # yes or no for each of the scalars.
+          comptable[rws & comptable[ , "totalscalar"] == 0, "totalscalar"] <- 1
+          comptable[rws & comptable[ , "delta"] == 0, "delta"] <- 1
 
+          # Finally record the deltas.
+          for (k in which(rws)) {
+            deltas[[k]] <- c(deltas[[k]], currentscale)
+          }
+        }
+        if (currenttrans == "Kappa") {
+          descs <- getDescs(extree, mrca)
+          rws <- comptable[ , 1] %in% descs
+          counts[counts[, "descNode"] %in% descs , "nScalar"] <- counts[counts[, "descNode"] %in% descs , "nScalar"] + 1
+          counts[counts[, "descNode"] %in% descs , "nKappa"] <- counts[counts[, "descNode"] %in% descs , "nKappa"] + 1
+          comptable[rws & comptable[ , "totalscalar"] == 0, "totalscalar"] <- 1
+          comptable[rws & comptable[ , "kappa"] == 0, "kappa"] <- 1
+          # Finally record the kappas.
+          for (k in which(rws)) {
+            kappas[[k]] <- c(kappas[[k]], currentscale)
+          }
+        }    
+        if (currenttrans == "Lambda") {
+          descs <- getDescs(extree, mrca)
+          rws <- comptable[ , 1] %in% descs
+          counts[counts[, "descNode"] %in% descs , "nScalar"] <- counts[counts[, "descNode"] %in% descs , "nScalar"] + 1
+          counts[counts[ , "descNode"] %in% descs , "nLambda"] <- counts[counts[, "descNode"] %in% descs , "nLambda"] + 1
+          comptable[rws & comptable[ , "totalscalar"] == 0, "totalscalar"] <- 1
+          comptable[rws & comptable[ , "lambda"] == 0, "lambda"] <- 1
+          # Finally record the lambdas.
+          for (k in which(rws)) {
+            lambdas[[k]] <- c(lambdas[[k]], currentscale)
+          }          
+        } 
+      }      
+
+      # Now put the yes/no data from comptable into counts in the times scaled/rate, delta etc. This counts the number of 
+      # ITERATIONS when scaling occurred  .
+      tmp <- counts[ , "descNode"] %in% comptable[comptable[ , "totalscalar"] == 1, "descNode"] 
+      counts[tmp, "itersScaled"] <- counts[tmp, "itersScaled"] + 1
+      
+      tmp <- counts[ , "descNode"] %in% comptable[comptable[ , "rate"] == 1, "descNode"]
+      counts[tmp, "itersRatescaled"] <- counts[tmp, "itersRatescaled"] + 1
+      
+      tmp <- counts[ , "descNode"] %in% comptable[comptable[ , "delta"] == 1, "descNode"]
+      counts[tmp, "itersDelta"] <- counts[tmp, "itersDelta"] + 1
+      
+      tmp <- counts[ , "descNode"] %in% comptable[comptable[ , "kappa"] == 1, "descNode"]
+      counts[tmp, "itersKappa"] <- counts[tmp, "itersKappa"] + 1
+          
+      tmp <- counts[ , "descNode"] %in% comptable[comptable[ , "lambda"] == 1, "descNode"]
+      counts[tmp, "itersLambda"] <- counts[tmp, "itersLambda"] + 1
+    }
+  } 
+  # Finally generate the descriptive stuff for the scalar values, and remove columns that are all zero.
+  # First add in 1 where there is a NULL.
+
+  for (i in 1:length(rates)) {
+    if (!is.null(rates[[i]])) {
+      dens <- density(rates[[i]])
+      counts[i, "rangeRate"] <- max(rates[[i]]) - min(rates[[i]])
+      counts[i, "lqRate"] <- quantile(rates[[i]])[2]
+      counts[i, "uqRate"] <- quantile(rates[[i]])[4]
+      counts[i, "meanRate"] <- mean(rates[[i]])
+      counts[i, "medianRate"] <- median(rates[[i]])
+      counts[i, "modeRate"] <- dens$x[which(dens$y == max(dens$y))]
+    }
   }
+
+  for (i in 1:length(deltas)) {
+    if (!is.null(deltas[[i]])) {
+      dens <- density(deltas[[i]])
+      counts[i, "rangeDelta"] <- max(deltas[[i]]) - min(deltas[[i]])
+      counts[i, "lqDelta"] <- quantile(deltas[[i]])[2]
+      counts[i, "uqDelta"] <- quantile(deltas[[i]])[4]
+      counts[i, "meanDelta"] <- mean(deltas[[i]])
+      counts[i, "medianDelta"] <- median(deltas[[i]])
+      counts[i, "modeDelta"] <- dens$x[which(dens$y == max(dens$y))]
+    }
+  }
+
+  for (i in 1:length(kappas)) {
+    if (!is.null(kappas[[i]])) {
+      dens <- density(kappas[[i]])
+      counts[i, "rangeKappa"] <- max(kappas[[i]]) - min(kappas[[i]])
+      counts[i, "lqKappa"] <- quantile(kappas[[i]])[2]
+      counts[i, "uqKappa"] <- quantile(kappas[[i]])[4]
+      counts[i, "meanKappa"] <- mean(kappas[[i]])
+      counts[i, "medianKappa"] <- median(kappas[[i]])
+      counts[i, "modeKappa"] <- dens$x[which(dens$y == max(dens$y))]
+    }
+  }
+
+  for (i in 1:length(lambdas)) {
+    if (!is.null(lambdas[[i]])) {
+      dens <- density(lambdas[[i]])
+      counts[i, "rangeLambda"] <- max(lambdas[[i]]) - min(lambdas[[i]])
+      counts[i, "lqLambda"] <- quantile(lambdas[[i]])[2]
+      counts[i, "uqLambda"] <- quantile(lambdas[[i]])[4]
+      counts[i, "meanLambda"] <- mean(lambdas[[i]])
+      counts[i, "medianLambda"] <- median(lambdas[[i]])
+      counts[i, "modeLambda"] <- dens$x[which(dens$y == max(dens$y))]
+    }
+  }
+
+  # Finally remove zero columns.
+  keeps <- NULL
+  for (i in 1:ncol(counts)) {
+    if (any(counts[ , i] != 0)) {
+      keeps <- c(keeps, i)
+    }
+  }
+
+  counts <- counts[ , keeps]
   return(counts)
 }
-
-
-### To add - 
-# 1) 
-
-
-# So this gives the number of times scaled, overall, per rate, and per transformation.
-# The mean rate scalar needs to be added - this should be calculated per branch per generation (in the case of two scalars) and then over all per branch.
-# A complication is that a rate scalar, then a delta transformation, is not really measureable or interpretable.
-# Perhaps a measure of whether the deltas are >1 or <1 is useful as well - although again, in conjunction with rates I am not sure this is useful either...
-
-# make a function to compare the branch lengths from the posterior to the original branch lengths as well - this could be done from the trees, or 
-# from the raw output.
-
-# Also there must be a way in which I can use the partition tree of the rj output, and then turn it into per-branch in a ape-style tree, this might be faster.
-
-
-
-
-
-
-
-
