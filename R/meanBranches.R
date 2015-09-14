69b628a632847af5b830e44abe7831c1cf29dc4d
hferg <h.ferguson.gow@gmail.com>
2015-09-14 16:00:26 +0100

    updates to include quartiles in post processing

--- R/meanBranches.R
+++ R/meanBranches.R
@@ -1,5 +1,4 @@
-#' meanbranches
-#'
+#' meanbranches#'
 #' Calculate the mean branch lengths from a BayesTraits RJ analysis posterior, when topology is fixed.
 #' @param reftree A tree that provides the reference topology (ideally the tree the analysis was run on)
 #' @param trees The posterior sample of stretched trees from which you want the mean branch lengths.
@@ -11,7 +10,8 @@ meanBranches <- function(reftree, trees, burnin = 0, thinning = 1) {
   
   trees <- trees[seq.int(burnin, length(trees), thinning)]
   
-  bls <- vector(mode = "numeric", length = length(reftree$edge.length))
+  #bls <- vector(mode = "numeric", length = length(reftree$edge.length))
+  bls <- matrix(nrow = length(reftree$edge.length), ncol = length(trees))
   
   for (i in 1:length(trees)) {
     tree <- ladderize(trees[[i]])
@@ -24,14 +24,31 @@ meanBranches <- function(reftree, trees, burnin = 0, thinning = 1) {
       stop(paste("Tree", i, "has a different topology to reference tree"))
     }
     
-    bls <- bls + tree$edge.length
+    bls[ , i] <- tree$edge.length
     
   }
   
-  bls <- bls / length(trees)
+  rangebl <- vector(mode = "numeric", length = nrow(bls)) 
+  quart25 <- vector(mode = "numeric", length = nrow(bls))
+  quart75 <- vector(mode = "numeric", length = nrow(bls))
+  branches <- vector(mode = "numeric", length(nrow(bls)))
+  for (i in 1:nrow(bls)) {
+    branches[i] <- mean(bls[i, ])
+  }
+
+  for (i in 1:ncol(bls)) {
+    bls[ , i] <- bls [ , i] / reftree$edge.length
+  }
+
+  for (i in 1:nrow(bls)) {
+    quarts <- sort(bls[i, ])
+    quart25[i] <- quarts[round(length(quarts) * 0.25)]
+    quart75[i] <- quarts[round(length(quarts) * 0.75)]
+    rangebl[i] <- max(bls[i, ]) - min(bls[i, ])
+  }
+
   restree <- reftree
-  restree$edge.length <- bls
-  res <- list(ogtree = reftree, meantree = restree)
+  restree$edge.length <- branches
+  res <- list(ogtree = reftree, meantree = restree, quartileranges = quarts, rangescalar = rangebl)
   return(res)
 }
-
