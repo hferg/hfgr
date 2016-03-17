bbabef2196ef7cde6d4bebc486578df993294964
hferg <h.ferguson.gow@gmail.com>
2016-03-17 15:24:54 +0000

    Added an iption to plotTree to allow for a title in the plot

--- R/plotTree.R
+++ R/plotTree.R
@@ -7,11 +7,12 @@
 #' @param nodes TRUE shows all node labels, FALSE supresses node labels (default), or a vecotr of which nodes to highlight.
 #' @param scale Logical - show the scale bar, or not?
 #' @param cex Scaling factor for tip labels and node labels - universal.
+#' @param main A string that will be the title on the plot (if desired)
 #' @name plotTree
 #' @export
 
-plotTree <- function(tree, tips = FALSE, nodes = NULL, scale = TRUE, cex = 1) {
-  plot(tree, show.tip.label = tips, cex = cex)
+plotTree <- function(tree, tips = FALSE, nodes = NULL, scale = TRUE, cex = 1, main = "") {
+  plot(tree, show.tip.label = tips, cex = cex, main = main)
   if (!is.null(nodes)) {
     if (is.numeric(nodes)) {
       nodelabels(node = nodes, cex = cex)
