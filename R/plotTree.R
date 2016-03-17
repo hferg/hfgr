5084d473a53da529d7d4c8b2a68dd16e49ca03e1
hferg <h.ferguson.gow@gmail.com>
2016-03-17 15:22:04 +0000

    Updated plotTree to allow specific nodes to be plotted with the nodes argument.

--- R/plotTree.R
+++ R/plotTree.R
@@ -4,16 +4,21 @@
 #' an axis easier and quicker.
 #' @param tree An object of class phylo
 #' @param tips Logical - show tip labels or not?
-#' @param nodes Logical - show nodel labels, or nor?
+#' @param nodes TRUE shows all node labels, FALSE supresses node labels (default), or a vecotr of which nodes to highlight.
 #' @param scale Logical - show the scale bar, or not?
 #' @param cex Scaling factor for tip labels and node labels - universal.
 #' @name plotTree
 #' @export
 
-plotTree <- function(tree, tips = FALSE, nodes = FALSE, scale = TRUE, cex = 1) {
+plotTree <- function(tree, tips = FALSE, nodes = NULL, scale = TRUE, cex = 1) {
   plot(tree, show.tip.label = tips, cex = cex)
-  if (nodes) {
+  if (!is.null(nodes)) {
+    if (is.numeric(nodes)) {
+      nodelabels(node = nodes, cex = cex)
+    } else if (nodes == FALSE) {}
+    else {
     nodelabels(cex = cex)
+    }
   }
   if (scale) {
     axisPhylo()
