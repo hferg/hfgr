d63f4e6c4bf937e07371dcfd849e26c3504508db
hferg <h.ferguson.gow@gmail.com>
2016-04-12 16:15:20 +0100

    changes to the cladeChangeSim to allow any transformed tree for comparison

--- R/cladeChangeSim.R
+++ R/cladeChangeSim.R
@@ -4,35 +4,72 @@
 #' @name cladeChangeSim
 #' @export
 
-cladeChangeSim <- function(tree, node, directional = "non", startval = 0, sigsq, plot = FALSE) {
+cladeChangeSim <- function(tree, reftree = NULL, node, mode = "BM", directional = "non", startval = 0, sigsq, 
+  plot = FALSE, increment = NULL, param.value = NULL) {
     # Identify all edges - getDesTips_edge returns tip LABELS descendent from the branch (edge)
     # As such, this gives you all of the branches in the clade.
     # HFG CHANGE HERE
     descs <- getDescs(tree, node)
     tips <- descs[which(descs <= length(tree$tip.label))]
     internal <- descs[which(descs > length(tree$tip.label))]
+   
+ 
     allbranches <- c(which((tree$edge[ , 2] == node)), which.edge(tree, tree$tip.label[tips]))
     res <- vector(mode = "list", length = length(allbranches))
     names(res) <- allbranches
 
-    for (i in allbranches) {
-      increment <- max(tree$edge.length[allbranches]) / 500
-      bl <- tree$edge.length[i]
-      leading <- which(tree$edge[ , 2] == tree$edge[i, 1])
+    if (is.null(reftree)) {
+      for (i in allbranches) {
+        
+        if (is.null(increment)) {
+          increment <- max(tree$edge.length[allbranches]) / 500
+        }
 
-      if (length(leading) == 0) {
-        strt <- startval
-      } else if (!leading %in% allbranches) {
-        strt <- startval
-      } else {
-        tmp <- res[[which(names(res) == leading)]]
-        strt <- tmp[nrow(tmp), 2]
+        bl <- tree$edge.length[i]
+        leading <- which(tree$edge[ , 2] == tree$edge[i, 1])
+
+        if (length(leading) == 0) {
+          strt <- startval
+        } else if (!leading %in% allbranches) {
+          strt <- startval
+        } else {
+          tmp <- res[[which(names(res) == leading)]]
+          strt <- tmp[nrow(tmp), 2]
+        }
+        
+        sim <- branchChangeSim(branchtime = bl, direction = directional, startval = strt, var = sigsq, increment = increment)      
+        res[[which(names(res) == i)]] <- sim
+        res[[which(names(res) == i)]][ , 1] <- res[[which(names(res) == i)]][ , 1] + getPL(tree, node = tree$edge[i, 1])
       }
-      
-      sim <- branchChangeSim(branchtime = bl, direction = directional, startval = strt, var = sigsq, increment = increment)      
-      res[[which(names(res) == i)]] <- sim
-      res[[which(names(res) == i)]][ , 1] <- res[[which(names(res) == i)]][ , 1] + getPL(tree, node = tree$edge[i, 1])
+    } else {
+      for (i in allbranches) {
+        
+        if (is.null(increment)) {
+          increment <- max(tree$edge.length[allbranches]) / 500
+        }
+
+        bl <- tree$edge.length[i]
+        refbl <- reftree$edge.length[i]
+        blrat <- refbl / bl
+        leading <- which(tree$edge[ , 2] == tree$edge[i, 1])
+
+        if (length(leading) == 0) {
+          strt <- startval
+        } else if (!leading %in% allbranches) {
+          strt <- startval
+        } else {
+          tmp <- res[[which(names(res) == leading)]]
+          strt <- tmp[nrow(tmp), 2]
+        }
+        
+        sim <- branchChangeSim(branchtime = bl, direction = directional, startval = strt, var = (sigsq * blrat), 
+            increment = increment)      
+        res[[which(names(res) == i)]] <- sim
+        res[[which(names(res) == i)]][ , 1] <- res[[which(names(res) == i)]][ , 1] + getPL(tree, node = tree$edge[i, 1])
+      }
     }
+
+
   test <- melt(res)
   part1 <- test[test$Var2 == 1, ]
   part2 <- test[test$Var2 == 2, ]
