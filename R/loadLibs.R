c29022a02b13e0c32adf3b2fd335b98a37d3ce1b
hferg <h.ferguson.gow@gmail.com>
2016-05-23 11:30:16 +0100

    added loadLibs to load useful libraries easily.

---
+++ R/loadLibs.R
@@ -0,0 +1,16 @@
+#' loadLibs
+#' 
+#' Just a shortcut function to load all of the usual libraries at once.
+#' @export
+#' @name loadLibs
+
+loadLibs <- function() {
+  library(ape)
+  library(ggplot2)
+  library(geiger)
+  library(phytools)
+  library(reshape2)
+  library(BTRTools)
+  library(caper)
+  library(TreeSim)
+}
