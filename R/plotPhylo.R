3ccb54becbac376db8af83e565520f31ea3393c9
hferg <h.ferguson.gow@gmail.com>
2016-06-08 11:17:27 +0100

    added in edge colour options to plotPhylo

--- R/plotPhylo.R
+++ R/plotPhylo.R
@@ -8,11 +8,13 @@
 #' @param scale Logical - show the scale bar, or not?
 #' @param cex Scaling factor for tip labels and node labels - universal.
 #' @param main A string that will be the title on the plot (if desired)
+#' @param edge.cols A vector of edge colours, if you want the edges to be coloured.
 #' @name plotPhylo
 #' @export
 
-plotPhylo <- function(tree, tips = FALSE, nodes = NULL, scale = TRUE, cex = 1, main = "") {
-  plot(tree, show.tip.label = tips, cex = cex, main = main)
+plotPhylo <- function(tree, tips = FALSE, nodes = NULL, scale = TRUE, cex = 1, main = "",
+  edge.cols = NULL) {
+  plot(tree, show.tip.label = tips, cex = cex, main = main, edge.cols = edge.cols)
   if (!is.null(nodes)) {
     if (is.numeric(nodes)) {
       nodelabels(node = nodes, cex = cex)
