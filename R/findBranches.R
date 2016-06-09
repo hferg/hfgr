e107da9381052177cba91d1d90010868e1f27cf1
hferg <h.ferguson.gow@gmail.com>
2016-06-09 16:06:08 +0100

    find branches now allows including or excluding the tail of the clade.

--- R/findBranches.R
+++ R/findBranches.R
@@ -2,13 +2,19 @@
 #' Finds the branch numbers that descend from a given node of a tree.
 #' @param tree An object of class phylo
 #' @param node The node the descendent branches of which are of interest
+#' @param tail Include the branch leading to the node of interest, or not?
 #' @name findBranches
 #' @export
 
-findBranches <- function(tree, node) {
+findBranches <- function(tree, node, tail = TRUE) {
   descs <- getDescs(tree, node)
   tips <- descs[which(descs <= length(tree$tip.label))]
   internal <- descs[which(descs > length(tree$tip.label))]
-  allbranches <- c(which((tree$edge[ , 2] == node)), which.edge(tree, tree$tip.label[tips]))
+  if (tail) {
+    allbranches <- c(which((tree$edge[ , 2] == node)), which.edge(tree, tree$tip.label[tips]))
+  } else {
+    allbranches <- c(which((tree$edge[ , 1] == node)), which.edge(tree, tree$tip.label[tips]))
+  }
+  
   return(allbranches)
 }
